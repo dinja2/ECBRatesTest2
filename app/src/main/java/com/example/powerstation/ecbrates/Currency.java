f1d2bee4adfd779735b1bd7cbcb09094b2e655d7
Dennis Grigorenko <ville2003@list.ru>
2016-01-06 13:17:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/powerstation/ecbrates/Currency.java
@@ -0,0 +1,18 @@
+package com.example.powerstation.ecbrates;
+
+/**
+ * Created by Denis on 05.01.2016.
+ */
+import java.util.HashMap;
+
+public class Currency extends HashMap<String, String> {
+    public static final String NAME = "name";
+    public static final String RATE = "rate";
+
+
+    public Currency(String name, String rate) {
+        super();
+        super.put(NAME, name);
+        super.put(RATE, rate);
+    }
+}
